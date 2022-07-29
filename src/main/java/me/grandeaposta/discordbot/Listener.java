81ec066597a0f42c90a85a0fdaa06ae6f98ecf09
owlmilo1 <angelomig13@gmail.com>
2022-07-28 23:28:36 -0300

    clear command with role, bot following the first user who enters in voice channel, bot following the last user who changes to other voice channel, bot leaving the voice channel when the last user leaves

--- src/main/java/me/grandeaposta/discordbot/Listener.java
+++ src/main/java/me/grandeaposta/discordbot/Listener.java
@@ -2,6 +2,10 @@ package me.grandeaposta.discordbot;
 
 import me.grandeaposta.discordbot.managers.ActivityStartManager;
 import me.grandeaposta.discordbot.managers.CommandManager;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceMoveEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.user.UserActivityStartEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -15,15 +19,59 @@ public class Listener extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent event) {
         String msg = event.getMessage().getContentRaw();
 
-        if(msg.startsWith(Config.get("prefix"))){
+        if (msg.startsWith(Config.get("prefix"))) {
             comManager.handle(event);
         }
-        
+
     }
 
     @Override
     public void onUserActivityStart(UserActivityStartEvent event) {
         actManager.handle(event);
     }
+
+    @Override
+    public void onGuildVoiceJoin(GuildVoiceJoinEvent event) {
+        
+        Member bot = event.getGuild().getSelfMember();
+
+        if (event.getMember().getUser().isBot()) {
+            return;
+        }
+
+        if (bot.getVoiceState().inAudioChannel()) {
+            return;
+        }
+
+        event.getGuild().getAudioManager().openAudioConnection(event.getMember().getVoiceState().getChannel());
+    }
+
+    @Override
+    public void onGuildVoiceLeave(GuildVoiceLeaveEvent event) {
+        if (event.getMember().getUser().isBot()) {
+            return;
+        }
+
+        if(event.getChannelLeft().getMembers().size() == 0){
+            return;
+        }
+
+        if (event.getChannelLeft().getMembers().size() == 1 && event.getChannelLeft().getMembers().get(0) == event.getGuild().getSelfMember()) {
+            event.getGuild().getAudioManager().closeAudioConnection();
+        }
+
+    }
+
+    @Override
+    public void onGuildVoiceMove(GuildVoiceMoveEvent event) {
+        if (event.getMember().getUser().isBot()) {
+            return;
+        }
+
+        if (event.getChannelLeft().getMembers().size() == 1 && event.getChannelLeft().getMembers().get(0) == event.getGuild().getSelfMember()) {
+            event.getGuild().getAudioManager().openAudioConnection(event.getMember().getVoiceState().getChannel());
+        }
+    }
+    
 
 }
--- src/main/java/me/grandeaposta/discordbot/command/commands/ClearCommand.java
+++ src/main/java/me/grandeaposta/discordbot/command/commands/ClearCommand.java
@@ -2,6 +2,7 @@ package me.grandeaposta.discordbot.command.commands;
 
 import java.util.List;
 
+import me.grandeaposta.discordbot.Config;
 import me.grandeaposta.discordbot.command.CommandContext;
 import me.grandeaposta.discordbot.command.ICommand;
 import net.dv8tion.jda.api.entities.Message;
@@ -16,18 +17,20 @@ public class ClearCommand implements ICommand {
 
     @Override
     public void handle(CommandContext ctx) {
-        MessageReceivedEvent event = ctx.getEvent();
-
         
-
-        List<String> args = ctx.getArgs();
-
-        if (!isNumber(args.get(0))) {
-            return;
+        if(ctx.getEvent().getMember().getRoles().toString().contains(Config.get("role_principal"))){
+            MessageReceivedEvent event = ctx.getEvent();
+    
+            List<String> args = ctx.getArgs();
+    
+            if (!isNumber(args.get(0))) {
+                return;
+            }
+    
+            List<Message> messageList = event.getChannel().getHistory().retrievePast(Integer.parseInt(args.get(0)) + 1).complete();
+            event.getGuildChannel().deleteMessages(messageList).queue();            
         }
 
-        List<Message> messageList = event.getChannel().getHistory().retrievePast(Integer.parseInt(args.get(0)) + 1).complete();
-        event.getGuildChannel().deleteMessages(messageList).queue();
     }
 
     public boolean isNumber(String msg) {
